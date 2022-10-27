2241902ded0bbfa8d6434d4a159d071d8db65811
kemencei.vivi <kemencei.vivi@gmail.com>
2022-10-27 11:11:55 +0200

    feat: material ui integrate, custom colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,30 @@
-import '../styles/globals.css'
-import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
-import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
-import type { AppProps } from 'next/app'
-import { useState } from 'react'
+import * as React from 'react';
+import Head from 'next/head';
+import {AppProps} from 'next/app';
+import {ThemeProvider} from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import {CacheProvider, EmotionCache} from '@emotion/react';
+import theme from '../design/theme/theme';
+import createEmotionCache from '../design/theme/createEmotionCache';
 
-function MyApp({
-  Component,
-  pageProps,
-}: AppProps<{
-  initialSession: Session,
-}>) {
-  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
 
-  return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <Component {...pageProps} />
-    </SessionContextProvider>
-  )
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
-export default MyApp
+
+export default function MyApp(props: MyAppProps) {
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
+    );
+}
