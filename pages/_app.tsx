573b7970e57a0b044df48b77be11de9f64ccc71d
Fábián Szabolcs <szabolcs2000@yahoo.com>
2022-11-01 20:35:38 +0100

    fix: revert supabse client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,20 @@ import {CacheProvider, EmotionCache} from '@emotion/react';
 import theme from '../design/theme/theme';
 import createEmotionCache from '../design/theme/createEmotionCache';
 import '@fontsource/lexend-deca';
+import { createBrowserSupabaseClient, Session } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 
 const clientSideEmotionCache = createEmotionCache();
 
 interface MyAppProps extends AppProps {
     emotionCache?: EmotionCache;
+    initialSession: Session;
 }
 
 export default function MyApp(props: MyAppProps) {
-    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+    const {Component, emotionCache = clientSideEmotionCache, initialSession, pageProps} = props;
+    const [supabaseClient] = React.useState(() => createBrowserSupabaseClient())
+
     return (
         <CacheProvider value={emotionCache}>
             <Head>
@@ -23,7 +28,10 @@ export default function MyApp(props: MyAppProps) {
             </Head>
             <ThemeProvider theme={theme}>
                 <CssBaseline/>
-                <Component {...pageProps} />
+                <SessionContextProvider supabaseClient={supabaseClient} 
+                        initialSession={initialSession}>
+                    <Component {...pageProps} />
+                </SessionContextProvider>
             </ThemeProvider>
         </CacheProvider>
     );
