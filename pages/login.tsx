bbee490d16936a7fdd45c8ca7ac5a386feeb4e6a
Fábián Szabolcs <szabolcs2000@yahoo.com>
2022-11-09 21:40:06 +0100

    fix: format code with prettier

--- pages/login.tsx
+++ pages/login.tsx
@@ -8,7 +8,7 @@ const Login: NextPage = () => {
 	const supaBaseClient = useSupabaseClient();
 
 	return (
-		<Grid sx={{minHeight: '100vh'}} container direction='column' alignItems='center' justifyContent='center' spacing={4}>
+		<Grid sx={{ minHeight: '100vh' }} container direction='column' alignItems='center' justifyContent='center' spacing={4}>
 			<Grid item>
 				<Typography variant='h3' sx={{ color: colors.dark.primary.p150 }}>
 					Bejelentkezés
@@ -18,7 +18,7 @@ const Login: NextPage = () => {
 				<Grid
 					container
 					spacing={0}
-                    boxShadow={8}
+					boxShadow={8}
 					justifyContent='center'
 					sx={{ width: '500px', paddingBottom: '20px', paddingTop: '20px', backgroundColor: '#424242', borderRadius: '10px' }}
 				>
@@ -30,41 +30,41 @@ const Login: NextPage = () => {
 								style: {
 									input: {
 										borderRadius: '10px',
-                                        borderColor: colors.dark.primary.p120,
+										borderColor: colors.dark.primary.p120,
 									},
 									button: {
 										borderRadius: '10px',
 									},
 									anchor: { color: colors.dark.primary.p120, textDecoration: 'none' },
-                                    label: {
-                                        color: colors.dark.primary.p120
-                                    },
-                                    message: {
-                                        color: 'red'
-                                    }
+									label: {
+										color: colors.dark.primary.p120,
+									},
+									message: {
+										color: 'red',
+									},
 								},
 							}}
-                            localization={{
-                                variables: {
-                                  sign_in: {
-                                    email_label: 'Email',
-                                    password_label: 'Jelszó',
-                                    email_input_placeholder: 'Email cím',
-                                    password_input_placeholder: 'Írja be jelszavát',
-                                    button_label: 'Bejelentkezés',
-                                    link_text: 'Már van profilja? Jelentkezzen be!'
-                                  },
-                                  forgotten_password: {
-                                    link_text: 'Elfelejtte a jelszavát?',
-                                    button_label: 'Jelszóvisszaállítás',
-                                    email_input_placeholder: 'Email cím',
-                                    email_label: 'Email'
-                                  },
-                                  sign_up: {
-                                    link_text: ''
-                                  },
-                                },
-                            }}
+							localization={{
+								variables: {
+									sign_in: {
+										email_label: 'Email',
+										password_label: 'Jelszó',
+										email_input_placeholder: 'Email cím',
+										password_input_placeholder: 'Írja be jelszavát',
+										button_label: 'Bejelentkezés',
+										link_text: 'Már van profilja? Jelentkezzen be!',
+									},
+									forgotten_password: {
+										link_text: 'Elfelejtte a jelszavát?',
+										button_label: 'Jelszóvisszaállítás',
+										email_input_placeholder: 'Email cím',
+										email_label: 'Email',
+									},
+									sign_up: {
+										link_text: '',
+									},
+								},
+							}}
 						/>
 					</Grid>
 				</Grid>
