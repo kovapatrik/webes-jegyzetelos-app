25db744d54d05ec1f7260d331425051ad15b333d
zsofiaschell <43221385+zsofiaschell@users.noreply.github.com>
2022-11-29 19:02:40 +0100

    feat: create tests

---
+++ .github/workflows/test.js
@@ -0,0 +1,9 @@
+// Add some unit test scenarios.
+
+// import doctests from '@supabase/doctest-js';
+
+// describe('Doctests', () => {
+//   // file paths are relative to root of directory
+//   doctest('src/sum.js');
+//   doctest('src/someOtherFile.js');
+// });
