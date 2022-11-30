30c5eed72bdc01988ad404c8ff5005a0bd6d9dfd
kemencei.vivi <kemencei.vivi@gmail.com>
2022-11-30 21:14:27 +0100

    feat: login screen design

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,115 +7,116 @@ import { NextPage } from 'next';
 import { GetBaseNoteGroup } from '../lib/note_group';
 
 const Home: NextPage = () => {
-
 	const supaBaseClient = useSupabaseClient();
 	const user = useUser();
 	const router = useRouter();
 
-	const [baseId, setBaseId] = useState<string|null|undefined>(null)
+	const [baseId, setBaseId] = useState<string | null | undefined>(null);
 	if (baseId) {
-		router.push(`/${baseId}`)	
+		router.push(`/${baseId}`);
 	}
 	useEffect(() => {
 		supaBaseClient.auth.onAuthStateChange(async (event, session) => {
-			if (event == "PASSWORD_RECOVERY") {
-				const newPassword = prompt("What would you like your new password to be?");
+			if (event == 'PASSWORD_RECOVERY') {
+				const newPassword = prompt('What would you like your new password to be?');
 				const { data, error } = await supaBaseClient.auth.updateUser({
 					password: newPassword!,
-				})
+				});
 
-				if (data) alert("Password updated successfully!")
-				if (error) alert("There was an error updating your password.")
+				if (data) alert('Password updated successfully!');
+				if (error) alert('There was an error updating your password.');
 			}
-		})
+		});
 
 		async function getData() {
-			const data = await GetBaseNoteGroup({user, supabaseServerClient: supaBaseClient});
+			const data = await GetBaseNoteGroup({ user, supabaseServerClient: supaBaseClient });
 			setBaseId(data?.id);
 		}
 
 		if (user) getData();
+	}, [supaBaseClient.auth, user]);
 
-	}, [supaBaseClient.auth, user])
-
-	
-
-	return (
-		!user ?
-			<Grid sx={{ minHeight: '100vh' }} container direction='column' alignItems='center' justifyContent='center' spacing={4}>
-				<Grid item>
-					<Typography variant='h3' sx={{ color: '#1C5E2E' }}>
-						Bejelentkezés
-					</Typography>
-				</Grid>
-				<Grid item>
-					<Grid
-						container
-						spacing={0}
-						boxShadow={8}
-						justifyContent='center'
-						sx={{
-							width: '500px',
-							paddingBottom: '20px',
-							paddingTop: '20px',
-							backgroundColor: '#424242',
-							borderRadius: '10px',
-						}}
-					>
-						<Grid item sx={{ padding: '0px', width: '300px' }}>
-							<Auth
-								supabaseClient={supaBaseClient}
-								appearance={{
-									theme: ThemeSupa,
-									style: {
-										input: {
-											borderRadius: '10px',
-											borderColor: '#50d465',
-										},
-										button: {
-											borderRadius: '10px',
-										},
-										anchor: { color: '#052029', textDecoration: 'none' },
-										label: {
-											color: '#052029',
-										},
-										message: {
-											color: 'red',
-										},
+	return !user ? (
+		<Grid
+			sx={{
+				margin: '0px',
+				height: '100vh',
+				backgroundColor: '#2e2e2e',
+			}}
+			container
+			direction='column'
+			alignItems='center'
+			justifyContent='center'
+			spacing={4}
+		>
+			<Grid item>
+				<Typography variant='h3' sx={{ color: '#3ab27b' }}>
+					Bejelentkezés
+				</Typography>
+			</Grid>
+			<Grid item>
+				<Grid
+					container
+					spacing={0}
+					boxShadow={8}
+					justifyContent='center'
+					sx={{
+						width: '500px',
+						paddingBottom: '20px',
+						paddingTop: '20px',
+						backgroundColor: '#424242',
+						borderRadius: '10px',
+					}}
+				>
+					<Grid item sx={{ padding: '0px', width: '300px' }}>
+						<Auth
+							supabaseClient={supaBaseClient}
+							appearance={{
+								theme: ThemeSupa,
+								style: {
+									input: {
+										borderRadius: '10px',
+										borderColor: '#50d465',
 									},
-								}}
-								localization={{
-									variables: {
-										sign_in: {
-											email_label: 'Email',
-											password_label: 'Jelszó',
-											// email_input_placeholder: 'Email cím',
-											// password_input_placeholder: 'Írja be jelszavát',
-											button_label: 'Bejelentkezés',
-											link_text: 'Már van profilja? Jelentkezzen be!',
-										},
-										forgotten_password: {
-											link_text: 'Elfelejtte a jelszavát?',
-											button_label: 'Jelszóvisszaállítás',
-											// email_input_placeholder: 'Email cím',
-											email_label: 'Email',
-										},
-										sign_up: {
-											link_text: '',
-										},
+									button: {
+										borderRadius: '10px',
 									},
-								}}
-							/>
-						</Grid>
+									anchor: { textDecoration: 'none' },
+
+									message: {
+										color: 'red',
+									},
+								},
+							}}
+							localization={{
+								variables: {
+									sign_in: {
+										email_label: 'Email',
+										password_label: 'Jelszó',
+										// email_input_placeholder: 'Email cím',
+										// password_input_placeholder: 'Írja be jelszavát',
+										button_label: 'Bejelentkezés',
+										link_text: 'Már van profilja? Jelentkezzen be!',
+									},
+									forgotten_password: {
+										link_text: 'Elfelejtte a jelszavát?',
+										button_label: 'Jelszóvisszaállítás',
+										// email_input_placeholder: 'Email cím',
+										email_label: 'Email',
+									},
+									sign_up: {
+										link_text: '',
+									},
+								},
+							}}
+						/>
 					</Grid>
 				</Grid>
 			</Grid>
-		: 
-		<Grid>
-			PILL
 		</Grid>
-	)
-}
-
+	) : (
+		<Grid>PILL</Grid>
+	);
+};
 
 export default Home;
